69e122c8d6c8f9e1636024af1ea696675ee900a1
abrilkatherine <abrilkatherinec@gmail.com>
2021-05-29 11:54:28 -0300

    Correcciones y mejoras en jugar ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,72 +1,126 @@
 import constantes as const
-from etapa3 import elegir_palabra
-from diccionario import devolver_diccionario
 
-diccionario = devolver_diccionario()
 
 def ofuscar_palabra(palabra, letras_adivinadas):
+    """
+    Ofusca las letras de la palabra que todavía no han sido adivinadas utilizando signos de pregunta (?)
+
+    Parámetros:
+    - palabra: palabra a ofuscar
+    - letras_adivinadas: letras que no deben ofuscarse
+    """
+
     return "".join(letra if letra in letras_adivinadas else const.SIGNO_PREGUNTA for letra in palabra)
 
+
 def mostrar_informacion(mensaje, palabra, letras_adivinadas, letras_erroneas):
+    """
+    TODO: Completar
+    """
+
     palabra = ofuscar_palabra(palabra, letras_adivinadas)
     print(f"\n{mensaje} → {palabra}  Aciertos: {len(letras_adivinadas)}  Desaciertos: {len(letras_erroneas)} - {letras_erroneas}")
 
+
 def pedir_letra(letras_usadas):
+    """
+    Solicita una letra al usuario realizando las siguientes validaciones:
+    - Se ingresó un único caracter válido (no numérico o especial)
+    - La letra no se utilizó en intentos anteriores
+    - La letra no es un caracter utilizado para finalizar el juego.
+
+    Devuelve la letra ingresada por el usuario en minúscula.
+
+    Parámetros:
+    - letras_usadas: Lista de letras ya utilizadas
+    """
+
     letra_valida = False
     letra = ""
 
     while not letra_valida and letra not in const.LETRAS_FIN:
         letra = input(const.MENSAJE_INPUT_LETRA)
+        letra = letra.lower()
 
         if len(letra) != 1 or not letra.isalpha():
             print(f"\n{const.MENSAJE_INGRESO_INVALIDO}")
+
         elif letra in letras_usadas:
             print(f"\n{const.MENSAJE_LETRA_INGRESADA}")
+
         else:
             letra_valida = True
-    return letra.lower()
 
-def seleccion_palabra(desea_letras):
-    if desea_letras == 'si':
-        cuantas_letras = input('Cuantas letras? ')
-        palabra_adivinar = elegir_palabra(diccionario, cuantas_letras)
+    return letra
+
+
+def tiene_intentos(letras_erroneas):
+    """
+    Devuelve True si el usuario cuenta con intentos disponibles, es decir,
+    si la cantidad de letras erróneas ingresadas es menor a la cantidad máxima de desaciertos permitidos.
+    """
+    return len(letras_erroneas) < const.MAXIMOS_DESACIERTOS_PERMITIDOS
+
+
+def finalizar_juego(letra):
+    """
+    Devuelve True si la letra ingresada por el usuario es una letra de fin utilizada para finalizar el juego.
+    """
+    return letra in const.LETRAS_FIN
+
+
+def juego_ganado(palabra, letras_adivinadas):
+    """
+    Devuelve True si el jugador adivinó todas las letras de la palabra.
+    """
+    return len(set(palabra)) == len(letras_adivinadas)
+
+
+def mostrar_mensaje_final(palabra, letras_adivinadas, letra):
+    """
+    TODO: Completar
+    """
+    mensaje = ""
+
+    if juego_ganado(palabra, letras_adivinadas):
+        mensaje = "¡Ganaste!"
+
+    elif letra in const.LETRAS_FIN:
+        mensaje = "Gracias por participar"
+    
     else:
-        palabra_adivinar = elegir_palabra(diccionario)
+        mensaje = f"Perdiste :(, la palabra era: {palabra}"
+    
+    print(mensaje)
 
-    return palabra_adivinar
-
-def continuar_jugando(SEGUIR_JUGANDO):
-    if SEGUIR_JUGANDO == "si" :
-        jugar_ahorcado(seleccion_palabra(input('Desea una cantidad de letras específica? (si/no) ')))
-    elif SEGUIR_JUGANDO == "no":
-        print("Gracias por jugar!!!")
-        print("Tu puntaje fue:", const.PUNTAJE_DEL_JUEGO)
-    else:
-        print(continuar_jugando(input(const.INTRUZCA_COMANDO_DE_NUEVO)))
 
 def jugar_ahorcado(palabra):
+    """
+    TODO: Completar
+    """
+
     letra = ""
     letras_adivinadas = []
     letras_erroneas = []
-    gano = False
+    puntaje = 0
 
-    mostrar_informacion(const.MENSAJE_INICIAL, palabra, letras_adivinadas, letras_erroneas)
+    mostrar_informacion(const.MENSAJE_INICIAL, palabra,letras_adivinadas, letras_erroneas)
 
-    while letra not in const.LETRAS_FIN and len(letras_erroneas) < 7 and not gano:
+    while not finalizar_juego(letra) and tiene_intentos(letras_erroneas) and not juego_ganado(palabra, letras_adivinadas):
         letra = pedir_letra(letras_adivinadas + letras_erroneas)
 
         if letra in palabra:
             letras_adivinadas.append(letra)
             mensaje = const.MENSAJE_ACIERTO
-            const.PUNTAJE_DEL_JUEGO += 10
+            puntaje += const.PUNTAJE_ACIERTO
+
         else:
             letras_erroneas.append(letra)
             mensaje = const.MENSAJE_DESACIERTO
-            const.PUNTAJE_DEL_JUEGO -= 5
+            puntaje += const.PUNTAJE_DESACIERTO
 
         mostrar_informacion(mensaje, palabra, letras_adivinadas, letras_erroneas)
-        gano = len(set(palabra)) == len(letras_adivinadas)
+    
+    mostrar_mensaje_final(palabra, letras_adivinadas, letra)
 
-    return f"¡Ganaste!", continuar_jugando(input(const.SEGUIR_JUGANDO)) if gano else print("Perdiste :(", "La palabra era:", palabra), continuar_jugando(input(const.SEGUIR_JUGANDO))
-
-jugar_ahorcado(seleccion_palabra(input('Desea una cantidad de letras específica? (si/no) ')))
+    return puntaje
--- constantes.py
+++ constantes.py
@@ -10,9 +10,10 @@ MENSAJE_INPUT_LETRA = "Ingrese letra: "
 SEGUIR_JUGANDO = "Desea seguir jugando (si o no): "
 INTRUZCA_COMANDO_DE_NUEVO = "No entiendo su respuesta, introduzca si o no: "
 
-
 LETRAS_FIN = ["FIN", "0"]
 
-PUNTAJE_DEL_JUEGO = 0
 LONGITUD_MINIMA_PALABRA = 5
 INDICE_CLAVE = 0
+MAXIMOS_DESACIERTOS_PERMITIDOS = 7
+PUNTAJE_ACIERTO = 10
+PUNTAJE_DESACIERTO = -5
